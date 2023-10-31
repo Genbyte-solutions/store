96c2852b6f6a3e1cf840125ac1117f992646a323
Helter <heltherv@gmail.com>
2023-10-31 14:24:01 -0500

    seccion de pagos realizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+// import './App.css';
+import Button_Modal from "./components/Button_Modal/Button_Modal";
+import Pay_Verification from "./components/Pay_Verification/Pay_Verification";
+import PopUp_Modal from './assets/animations/PopUp_Modal';
 
 function App() {
   return (
     <>
-
+      <PopUp_Modal/>
+      <Button_Modal/>
     </>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+const Button_Pay = () => {
+    return (
+        <div className='py-3 d-flex justify-content-center'>
+            <button type='button' className='btn btn-success '>Inicio</button>
+        </div>
+    )
+}
+
+export default Button_Pay;
