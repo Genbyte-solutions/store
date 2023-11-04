fc4d7777d9f6e1b01052446139311753b90d0bc3
Helter <heltherv@gmail.com>
2023-11-04 13:56:44 -0500

    Approved or rejected payment section, made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-// import './App.css';
-import Button_Modal from "./components/Button_Modal/Button_Modal";
-import Pay_Verification from "./components/Pay_Verification/Pay_Verification";
-import PopUp_Modal from './assets/animations/PopUp_Modal';
+
 
 function App() {
   return (
     <>
-      <PopUp_Modal/>
-      <Button_Modal/>
+      
     </>
   )
 }
