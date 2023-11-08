e20fe3048d7f4eb0e17f3dcfd6b1b9fd10139990
Lautaro073 <lautarojimene02@gmail.com>
2023-11-07 22:15:38 -0300

    añadir page

--- frontend/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button_Pay = () => {
-    return (
-        <div className='py-3 d-flex justify-content-center'>
-            <button type='button' className='btn btn-success '>Inicio</button>
-        </div>
-    )
-}
-
-export default Button_Pay;
